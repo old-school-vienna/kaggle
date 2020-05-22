5a8c2f49859cfabc7680fb52c91c6f1ea8ac4376
wwagner4 <wwagner4@gmail.com>
2020-05-22 09:07:17 +0200

    reorganized data dir

--- m5fa/pyspark/analyse/README.md
+++ m5fa/pyspark/analyse/README.md
@@ -1,11 +1,20 @@
 ## Analyse data using pyspark
 ### Docker
 ```
-docker build -t myspark:xx .
+docker build -t myspark .
+```
+Add the second mount if original is a link
+```
+docker run -it \
+    -e DATADIR=/opt/data \
+    -v /home/wwagner4/prj/kaggle/m5fa/pyspark/analyse:/opt/project \
+    -v /data/kaggle:/opt/data \
+    myspark bash
 ```
 
 ### Data
 Copy the original data from kaggle into data/original. They are too big for git.
 ```
-https://www.kaggle.com/c/m5-forecasting-accuracy/data
+kaggle: https://www.kaggle.com/c/m5-forecasting-accuracy/data
+drive: https://drive.google.com/drive/folders/1koGlsp1fcV0JsPvIUtrFS1E-UhMHV2aa
 ```
--- m5fa/pyspark/analyse/csv.py
+++ m5fa/pyspark/analyse/csv.py
@@ -1,11 +1,14 @@
+import os
 from typing import List
 
 from pyspark import RDD
 from pyspark.shell import spark, sc
 from pyspark.sql import DataFrame
 
+datadir: str = os.getenv("DATADIR")
 
-def vars(prefix: str, fr: int, to: int) -> RDD:
+
+def tvars(prefix: str, fr: int, to: int) -> RDD:
     data = range(fr, to + 1)
     distData: RDD = sc.parallelize(data)
     return distData \
@@ -17,13 +20,13 @@ def varnams(vars: RDD) -> List[str]:
     return vars.map(lambda x: x[1]).collect()
 
 
-t_vars_all = vars("d_", 1, 1913)
-t_vars_small = vars("d_", 1, 15)
+t_vars_all = tvars("d_", 1, 1913)
+t_vars_small = tvars("d_", 1, 15)
 
 grp_vars = ["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"]
 print(grp_vars)
 
-train: DataFrame = spark.read.csv("data/original/sales_train_validation.csv", header=True)
+train: DataFrame = spark.read.csv(f"{datadir}/sales_train_validation.csv", header=True)
 
 train1 = train[grp_vars] \
     .sort('item_id', 'state_id')
