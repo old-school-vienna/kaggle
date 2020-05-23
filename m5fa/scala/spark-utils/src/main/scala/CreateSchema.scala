29d7ca87f288dee22599d836b7d81f324a201048
wwagner4 <wwagner4@gmail.com>
2020-05-23 09:04:47 +0200

    creating schema ok

--- m5fa/scala/spark-utils/src/main/scala/CreateSchema.scala
+++ m5fa/scala/spark-utils/src/main/scala/CreateSchema.scala
@@ -1,7 +1,34 @@
+import java.nio.file.{Path, Paths}
+
+import org.apache.spark.sql.SparkSession
+
 object CreateSchema {
-  
+
   def main(args: Array[String]): Unit = {
-    print("Hi spark")
+    val fileNam = "Sales5_Ab2011_InklPred.csv"
+
+    val spark = SparkSession.builder.appName("Simple Application")
+      .config("spark.master", "local")
+      .getOrCreate()
+    val datadir = System.getenv("DATADIR")
+    if (datadir == null) throw new IllegalArgumentException("Environment variable DATADIR must be defined")
+    val fp = Paths.get(datadir, fileNam)
+
+    val df = spark.read.option("header", "true").csv(fp.toString)
+
+    val row = df.first()
+    val indent1 = "      "
+    val indent2 = "            "
+    println()
+    println(s"${indent1}schema = T.StructType([")
+    val fields = for (f <- row.schema) yield {
+      val strNullable = if (f.nullable) "True" else "False"
+      s"${indent2}T.StructField('${f.name}', T.${f.dataType}(), $strNullable)"
+    }
+    println(fields.mkString(",\n"))
+    println(s"$indent1])")
+    println()
+
   }
 
 }
