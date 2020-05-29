5fc50505710daf5d74d611658774ca2a3bfe9a0c
wwagner4 <wwagner4@gmail.com>
2020-05-29 08:45:49 +0200

    measure time

---
+++ m5fa/pyspark/analyse/karl03.py
@@ -0,0 +1,65 @@
+import os
+import time
+from pathlib import Path
+
+import pyspark.sql.types as T
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer, OneHotEncoderEstimator
+from pyspark.sql import DataFrame, SparkSession
+
+
+def run():
+    spark = SparkSession.builder \
+        .appName("karl02") \
+        .getOrCreate()
+
+    datadir: str = os.getenv("DATADIR")
+    if datadir is None:
+        raise ValueError("Environment variable DATADIR must be defined")
+    print(f"datadir = '{datadir}'")
+
+    schema = T.StructType([
+        T.StructField('year', T.IntegerType(), True),
+        T.StructField('month', T.IntegerType(), True),
+        T.StructField('dn', T.IntegerType(), True),
+        T.StructField('wday', T.IntegerType(), True),
+        T.StructField('snap', T.IntegerType(), True),
+        T.StructField('dept_id', T.StringType(), True),
+        T.StructField('item_id', T.StringType(), True),
+        T.StructField('store_id', T.StringType(), True),
+        T.StructField('sales', T.DoubleType(), True),
+        T.StructField('flag_ram', T.IntegerType(), True),
+        T.StructField('Sales_Pred', T.DoubleType(), True)
+    ])
+
+    p = str(Path(datadir, "Sales5_Ab2011_InklPred.csv"))
+    print(f"Reading: '{p}'")
+
+    train: DataFrame = spark.read.csv(p, header='true', schema=schema)
+    # apply dummy variables
+
+    catvars = ['dept_id', 'item_id', 'wday']
+
+    stages = []
+
+    for v in catvars:
+        stages += [StringIndexer(inputCol=v,
+                                 outputCol=f"i{v}")]
+    ohin = [f"i{v}" for v in catvars]
+    ohout = [f"v{v}" for v in catvars]
+    stages += [OneHotEncoderEstimator(inputCols=ohin,
+                                      outputCols=ohout)]
+
+    pip = Pipeline(stages=stages)
+    pipm = pip.fit(train)
+
+    dft = pipm.transform(train)
+    dft.show()
+
+
+start = time.time()
+run()
+end = time.time()
+elapsed = end - start
+elapseds = time.strftime("%H:%M:%S", time.gmtime(elapsed))
+print(f"------------------------- R E A D Y ------------ {elapseds} --------------------")
