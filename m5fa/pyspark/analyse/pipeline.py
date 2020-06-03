7fe1df096a9ff14b046ada861212b1334acfa323
wwagner4 <wwagner4@gmail.com>
2020-06-03 08:21:09 +0200

    random split added

--- m5fa/pyspark/analyse/pipeline.py
+++ m5fa/pyspark/analyse/pipeline.py
@@ -67,7 +67,40 @@ def pipeline(spark: SparkSession, pppath: Path, datadir: Path):
     print(f"--- Reading: '{pppath}'")
 
     pp: DataFrame = spark.read.parquet(str(pppath))
-    pp.show()
+    from pyspark.ml.evaluation import RegressionEvaluator
+    from pyspark.ml.regression import LinearRegression
+    from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
+
+    # Prepare training and test data.
+    train, test = pp.randomSplit([0.9, 0.1], seed=12345)
+
+    lr = LinearRegression(maxIter=10)
+
+    # We use a ParamGridBuilder to construct a grid of parameters to search over.
+    # TrainValidationSplit will try all combinations of values and determine best model using
+    # the evaluator.
+    paramGrid = ParamGridBuilder()\
+        .addGrid(lr.regParam, [0.1, 0.01]) \
+        .addGrid(lr.fitIntercept, [False, True])\
+        .addGrid(lr.elasticNetParam, [0.0, 0.5, 1.0])\
+        .build()
+
+    # In this case the estimator is simply the linear regression.
+    # A TrainValidationSplit requires an Estimator, a set of Estimator ParamMaps, and an Evaluator.
+    tvs = TrainValidationSplit(estimator=lr,
+                            estimatorParamMaps=paramGrid,
+                            evaluator=RegressionEvaluator(),
+                            # 80% of the data will be used for training, 20% for validation.
+                            trainRatio=0.8)
+
+    # Run TrainValidationSplit, and choose the best set of parameters.
+    model = tvs.fit(train)
+
+    # Make predictions on test data. model is the model with combination of parameters
+    # that performed best.
+    model.transform(test)\
+        .select("features", "label", "prediction")\
+        .show()
 
 
 def run(spark: SparkSession):
