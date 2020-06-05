3ed64cc0250d383ebd2feb27224dcd350458e5a3
wwagner4 <wwagner4@gmail.com>
2020-06-05 18:48:11 +0200

    added todo

--- m5fa/pyspark/analyse/pipeline.py
+++ m5fa/pyspark/analyse/pipeline.py
@@ -1,15 +1,16 @@
 import os
 import time
 from pathlib import Path
+from typing import Any
 
-import pyspark.sql.types as t
-import pyspark.sql.functions as f
+import pyspark.sql.functions as sfunc
+import pyspark.sql.types as stype
 from pyspark.ml import Pipeline, Estimator
-from pyspark.ml.feature import StringIndexer, OneHotEncoderEstimator, VectorAssembler
-from pyspark.sql import DataFrame, SparkSession
 from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.regression import LinearRegression
+from pyspark.ml.feature import StringIndexer, OneHotEncoderEstimator, VectorAssembler
+from pyspark.ml.regression import GeneralizedLinearRegression
 from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
+from pyspark.sql import DataFrame, SparkSession
 
 
 def preprocessing(spark: SparkSession, pppath: Path, datadir: Path):
@@ -32,25 +33,25 @@ def preprocessing(spark: SparkSession, pppath: Path, datadir: Path):
 
     print("--- preprocessing -----------------------")
 
-    schema = t.StructType([
-        t.StructField('year', t.IntegerType(), True),
-        t.StructField('month', t.IntegerType(), True),
-        t.StructField('dn', t.IntegerType(), True),
-        t.StructField('wday', t.IntegerType(), True),
-        t.StructField('snap', t.IntegerType(), True),
-        t.StructField('dept_id', t.StringType(), True),
-        t.StructField('item_id', t.StringType(), True),
-        t.StructField('store_id', t.StringType(), True),
-        t.StructField('sales', t.DoubleType(), True),
-        t.StructField('flag_ram', t.IntegerType(), True),
-        t.StructField('Sales_Pred', t.DoubleType(), True)
+    schema = stype.StructType([
+        stype.StructField('year', stype.IntegerType(), True),
+        stype.StructField('month', stype.IntegerType(), True),
+        stype.StructField('dn', stype.IntegerType(), True),
+        stype.StructField('wday', stype.IntegerType(), True),
+        stype.StructField('snap', stype.IntegerType(), True),
+        stype.StructField('dept_id', stype.StringType(), True),
+        stype.StructField('item_id', stype.StringType(), True),
+        stype.StructField('store_id', stype.StringType(), True),
+        stype.StructField('sales', stype.DoubleType(), True),
+        stype.StructField('flag_ram', stype.IntegerType(), True),
+        stype.StructField('Sales_Pred', stype.DoubleType(), True)
     ])
 
     csv_path = datadir / "Sales5_Ab2011_InklPred.csv"
     print(f"--- Reading: '{csv_path}'")
 
     sales5: DataFrame = spark.read.csv(str(csv_path), header='true', schema=schema) \
-        .withColumn("label", f.col('sales'))
+        .withColumn("label", sfunc.col('sales'))
 
     ppdf = prepro(sales5)
     print(f"--- Writing: '{pppath}'")
@@ -60,11 +61,18 @@ def preprocessing(spark: SparkSession, pppath: Path, datadir: Path):
         .save(str(pppath))
 
 
-def pipeline1(pp: DataFrame, esti: Estimator) -> float:
-    return 0.6475
+def pipeline1(pp: DataFrame, esti: Estimator) -> (float, dict):
+    marams = {
+        'p1': 298347,
+        'hste_la_vista': 'H',
+        'good': True,
+        'value_up': 8.2347,
+        'nix': None
+    }
+    return 7.9238429847, marams
 
 
-def pipeline(pp: DataFrame, esti: Estimator) -> float:
+def pipeline(pp: DataFrame, esti: Estimator) -> (float, dict):
     # Prepare training and test data.
     train, test = pp.randomSplit([0.9, 0.1], seed=12345)
 
@@ -74,7 +82,6 @@ def pipeline(pp: DataFrame, esti: Estimator) -> float:
     param_grid = ParamGridBuilder() \
         .addGrid(esti.regParam, [0.1, 0.01]) \
         .addGrid(esti.fitIntercept, [False, True]) \
-        .addGrid(esti.elasticNetParam, [0.0, 0.5, 1.0]) \
         .build()
 
     # In this case the estimator is simply the linear regression.
@@ -86,19 +93,21 @@ def pipeline(pp: DataFrame, esti: Estimator) -> float:
                                trainRatio=0.8)
 
     # Run TrainValidationSplit, and choose the best set of parameters.
-    esti = tvs.fit(train)
+    besti = tvs.fit(train)
+    param_map = besti.bestModel.extractParamMap()
 
     # Make predictions on test data. model is the model with combination of parameters
     # that performed best.
-    predictions = esti.transform(test) \
+    predictions = besti.transform(test) \
         .select("features", "label", "prediction")
 
     # Select (prediction, true label) and compute test error
     evaluator = RegressionEvaluator(
         labelCol="label", predictionCol="prediction", metricName="rmse")
     rmse = evaluator.evaluate(predictions)
+
     print("Root Mean Squared Error (RMSE) on test data = %g" % rmse)
-    return rmse
+    return rmse, param_map
 
 
 def read_data(spark: SparkSession) -> DataFrame:
@@ -119,18 +128,61 @@ def read_data(spark: SparkSession) -> DataFrame:
 def main():
     start = time.time()
     spark = SparkSession.builder \
-        .appName(__name__) \
+        .appName(os.path.basename(__file__)) \
         .getOrCreate()
     pp: DataFrame = read_data(spark)
-    estis = [(pp, LinearRegression(maxIter=10), "lr max iter 10")]
-    mses = [(pipeline1(t[0], t[1]), t[2]) for t in estis]
-    print(mses)
+    estis1 = [
+        (pp, GeneralizedLinearRegression(family='poisson', link='identity', maxIter=10, regParam=0.3),
+         "glr poisson identity"), "glrpi",
+    ]
+    estis2 = [
+        (pp, GeneralizedLinearRegression(family='gaussian', link='identity', maxIter=10, regParam=0.3),
+         "glr gaussian identity", "glrgi"),
+        (pp, GeneralizedLinearRegression(family='poisson', link='log', maxIter=10, regParam=0.3),
+         "glr poisson log", "glrpi"),
+        (pp, GeneralizedLinearRegression(family='binomial', link='logit', maxIter=10, regParam=0.3),
+         "glr gamma identity", "glrgai"),
+    ]
+    reses = [(pipeline(t[0], t[1]), t[2], t[3]) for t in estis1]
+    print()
+    print(f"+------------------------------------------------+")
+    print(f"|model                                |mse       |")
+    print(f"|------------------------------------------------|")
+    for res in reses:
+        print(f"|{res[1]:37}|{res[0][0]:10.4f}|")
+    print(f"+------------------------------------------------+")
+    print()
+    for res in reses:
+        print(f"Hyperparams: {res[1]}")
+        hpar: dict = res[0][1]
+        print(f"+------------------------------------------------+")
+        for k in hpar.keys():
+            v = hpar[k]
+            print(f"|{k:37}|{fval(v, 10)}|")
+            print(f"+------------------------------------------------+")
+        print()
+    print()
     end = time.time()
     elapsed = end - start
     elapseds = time.strftime("%H:%M:%S", time.gmtime(elapsed))
     print(f"------------------------- R E A D Y ------------ {elapseds} --------------------")
     spark.stop()
     exit(0)
+
+
+def fval(value: Any, leng: int) -> str:
+    if value is None:
+        fstr = f"{{:{leng}}}"
+        return fstr.format('None')
+    if isinstance(value, int):
+        fstr = f"{{:{leng}d}}"
+        return fstr.format(value)
+    if isinstance(value, float):
+        fstr = f"{{:{leng}.3f}}"
+        return fstr.format(value)
+    else:
+        fstr = f"{{:{leng}}}"
+        return fstr.format(str(value))
 
 
 if __name__ == '__main__':
