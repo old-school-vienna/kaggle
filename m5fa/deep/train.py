29eda59319c6ad30ce36acdace40dd44c47c0848
wwagner <wolfgang.wagner@itsv.at>
2020-06-27 12:36:09 +0200

    generic net test OK

--- m5fa/deep/train.py
+++ m5fa/deep/train.py
@@ -11,40 +11,25 @@ from tensorflow.keras import layers
 import helpers as hlp
 
 
-def build_model0(num_input: int, num_output: int):
-    mo = keras.Sequential([
-        layers.Dense(num_input, activation='relu', input_shape=[num_input]),
-        layers.Dense(num_output, activation='relu'),
-        layers.Dense(num_output, activation='relu'),
-        layers.Dense(num_output, activation='relu'),
-        layers.Dense(num_output, activation='relu'),
-        layers.Dense(num_output)
-    ])
-    optimizer = tf.keras.optimizers.RMSprop(0.001)
-    mo.compile(loss='mse',
-               optimizer=optimizer,
-               metrics=['mae', 'mse'])
-    return mo
+def nodes(fact: float, n: int) -> int:
+    return int(n * fact)
 
 
-def build_model1(num_input: int, num_output: int):
-    mo = keras.Sequential()
-    mo.add(layers.Dense(num_input, activation='relu', input_shape=[num_input]))
-    mo.add(layers.Dense(num_output, activation='relu'))
-    mo.add(layers.Dense(num_output, activation='relu'))
-    mo.add(layers.Dense(num_output, activation='relu'))
-    mo.add(layers.Dense(num_output, activation='relu'))
-    mo.add(layers.Dense(num_output))
-    optimizer = tf.keras.optimizers.RMSprop(0.001)
-    mo.compile(loss='mse',
-               optimizer=optimizer,
-               metrics=['mae', 'mse'])
-    return mo
+def nnam(net, nin: int, nout: int) -> str:
+    def nam1(li, n) -> list:
+        def nodes1(f: float) -> str:
+            non = nodes(f, n)
+            if non > 1000000:
+                return f"{int(non / 1000000)}m"
+            elif non > 1000:
+                return f"{int(non / 1000)}k"
+            else:
+                return f"{non}"
 
+        return [nodes1(x) for x in li]
 
-def build_model2(num_input: int, num_output: int):
-    return build_model_gen(num_input, num_output, ([], [1.0, 1.0, 1.0, 1.0]), 0.001)
-    # return build_model_gen(num_input, num_output, [], [1.0], 0.001)
+    alln = nam1(net[0], nin) + nam1(net[1], nout)
+    return '-'.join(alln)
 
 
 def build_model_gen(num_input: int, num_output: int, net: tuple, stepw: float):
@@ -52,10 +37,10 @@ def build_model_gen(num_input: int, num_output: int, net: tuple, stepw: float):
 
     mo.add(layers.Dense(num_input, activation='relu', input_shape=[num_input]))
     for f in net[0]:
-        n = int(f * num_input)
+        n = nodes(f, num_input)
         mo.add(layers.Dense(n, activation='relu'))
     for f in net[1]:
-        n = int(f * num_input)
+        n = nodes(f, num_output)
         mo.add(layers.Dense(n, activation='relu'))
     mo.add(layers.Dense(num_output))
 
@@ -90,26 +75,51 @@ def train(sp: SparkSession):
     print(f"-- test data: {test_data.shape}")
     print(f"-- test labels: {test_labels.shape}")
     print("-------------------------------------------------------------------------------")
+    nin = len(xvars)
+    nout = len(yvars)
 
-    epochs = 1000
+    stepw = 0.001
+    nets = [
+        ([], [1.]),
+        ([], [0.5, 1.]),
+        ([], [1., 1.]),
+        ([], [1., 1., 1.]),
+        ([], [1., 1., 1., 1.]),
+        ([], [1., 1., 1., 1., 1.]),
+        ([1.], [1., 1.]),
+        ([1., 2.], [1., 1.]),
+        ([1., 2., 2.], [1., 1.]),
+    ]
+    results = []
+    for net in nets:
+        mse = train1(net, stepw, nin, nout, test_data, test_labels, train_data, train_labels)
+        results.append((nnam(net, nin, nout), mse))
+
+    print("----RESULTS (final)---------------------------------------------------------------")
+    for nam, mse in results:
+        print(f"-- {nam:50} - {mse:10.4f}")
+    print("----------------------------------------------------------------------------------")
+
+
+def train1(net: tuple, stepw: float,
+           nin: int, nout: int,
+           test_data: pd.DataFrame, test_labels: pd.DataFrame,
+           train_data: pd.DataFrame, train_labels: pd.DataFrame) -> float:
+    model = build_model_gen(nin, nout, net, stepw=stepw)
     cb = tf.keras.callbacks.EarlyStopping(
         monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto',
         baseline=None, restore_best_weights=False
     )
-
-    model = build_model2(len(xvars), len(yvars))
     history = model.fit(
         train_data, train_labels,
-        epochs=epochs, validation_split=0.2, verbose=0,
+        epochs=1000, validation_split=0.2, verbose=0,
         callbacks=[cb],
     )
     print("----HISTORY------------------------------------------------------------")
     tmses = history.history['mse']
     print(f"-- train mse {len(tmses)} {tmses}")
     print("-----------------------------------------------------------------------")
-
     test_predictions = model.predict(test_data)
-
     mse = ((test_labels.values - test_predictions) ** 2).mean(axis=0)
     msem = numpy.mean(mse)
     mses = numpy.sum(mse)
@@ -117,6 +127,7 @@ def train(sp: SparkSession):
     print(f"-- mse mean: {msem:10.4f}")
     print(f"-- mse sum:  {mses:10.4f}")
     print("----------------------------------------------------------------------")
+    return msem
 
 
 if __name__ == "__main__":
