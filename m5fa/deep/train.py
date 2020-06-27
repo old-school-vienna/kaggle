1dede54a66dfc3c038ff6a3d6e8f246a104a9163
wwagner <wolfgang.wagner@itsv.at>
2020-06-27 11:20:26 +0200

    refactoring before generic model

--- m5fa/deep/train.py
+++ m5fa/deep/train.py
@@ -66,34 +66,30 @@ def build_model_gen(num_input: int, num_output: int, ifac_in: List[float], ifac_
     return mo
 
 
-def train(spark: SparkSession):
-    df: pd.DataFrame = hlp.readFromDatadirParquet(spark, 'sp5_02').toPandas()
-
+def train(sp: SparkSession):
+    df: pd.DataFrame = hlp.readFromDatadirParquet(sp, 'sp5_02').toPandas()
     df = df.astype(float)
 
     train_dataset = df.sample(frac=0.8, random_state=0)
     test_dataset = df.drop(train_dataset.index)
 
-    print(train_dataset.shape)
-    print(test_dataset.shape)
-
     allvars = df.keys()
     xvars = ['dn', 'flag_ram', 'month', 'snap', 'vdept_id_0', 'vdept_id_1',
              'vwday_0', 'vwday_1', 'vwday_2', 'vwday_3', 'vwday_4', 'vwday_5', 'year']
     yvars = [x for x in allvars if x not in xvars]
-    pprint(xvars)
-    pprint(yvars)
+    pprint(f"-- predictors X: {xvars}")
+    pprint(f"-- labels     y: {yvars}")
 
     train_data = train_dataset[xvars]
     train_labels = train_dataset[yvars]
     test_data = test_dataset[xvars]
     test_labels = test_dataset[yvars]
-    print("data")
-    print(train_data.shape)
-    print(test_data.shape)
-    print("labels")
-    print(train_labels.shape)
-    print(test_labels.shape)
+    print("----DATA-----------------------------------------------------------------------")
+    print(f"-- train data: {train_data.shape}")
+    print(f"-- train labels: {train_labels.shape}")
+    print(f"-- test data: {test_data.shape}")
+    print(f"-- test labels: {test_labels.shape}")
+    print("-------------------------------------------------------------------------------")
 
     epochs = 1000
     cb = tf.keras.callbacks.EarlyStopping(
@@ -108,16 +104,19 @@ def train(spark: SparkSession):
         callbacks=[cb],
     )
     print("----HISTORY------------------------------------------------------------")
-    for h in history.history['mse']:
-        pprint(h)
+    tmses = history.history['mse']
+    pprint(f"-- train mse {len(tmses)} {tmses}")
     print("-----------------------------------------------------------------------")
+
     test_predictions = model.predict(test_data)
 
-    pprint(test_labels.shape)
-    pprint(test_predictions.shape)
-
     mse = ((test_labels.values - test_predictions) ** 2).mean(axis=0)
-    print(f"-- mse {numpy.mean(mse)}")
+    msem = numpy.mean(mse)
+    mses = numpy.sum(mse)
+    print("----RESULT------------------------------------------------------------")
+    print(f"-- mse mean: {msem:10.4f}")
+    print(f"-- mse sum:  {mses:10.4f}")
+    print("----------------------------------------------------------------------")
 
 
 if __name__ == "__main__":
